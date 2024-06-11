111b26fa658e1f4b2b9c911d53026d20a23d5a37
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-11 09:13:05 +0700

    first commit

---
+++ src/test/java/java/thread/core/BelajarJavaThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package java.thread.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
